d84690486bacc1f3f229359ea2dc8a644376c373
ftardasti96 <f.tardasti@yahoo.com>
2019-08-06 14:19:10 +0430

    delete mvp added

--- create_customer/views.py
+++ create_customer/views.py
@@ -3,22 +3,33 @@ from .models import PersonalData
 from .forms import PersonalForm
 
 # Create your views here.
+
+
 def new_customer(request):
     form = PersonalForm(request.POST)
     if form.is_valid():
         form.save()
-        return render(request,'customerform/thanks.html')
-    return render(request, 'customerform/index.html',{'form':form})
+        return render(request, 'customerform/thanks.html')
+    return render(request, 'customerform/index.html', {'form': form})
+
 
 def home(request):
-    return render(request,'home/home.html')
+    return render(request, 'home/home.html')
 
 
 def list(request):
-    name = PersonalData.objects.order_by('id')
-    return render(request,'list/all.html',{'name':name})
+    if request.method == 'GET':
+        name = PersonalData.objects.order_by('id')
+        return render(request, 'list/all.html', {'name': name})
 
 
-def detail(request,pk):
+def detail(request, pk):
     user = PersonalData.objects.get(pk=pk)
-    return render(request,'list/detail.html',{'user':user})
+    return render(request, 'list/detail.html', {'user': user})
+
+
+def delete_customer(request, pk):
+    user = PersonalData.objects.get(pk=pk)
+    user.delete()
+    name = PersonalData.objects.all()
+    return render(request, 'list/all.html', {'name': name})
