c676524b338dc8ac22c5cff93e1de273e69860a7
ftardasti96 <f.tardasti@yahoo.com>
2019-08-06 10:21:03 +0430

    list added

--- create_customer/views.py
+++ create_customer/views.py
@@ -10,3 +10,15 @@ def new_customer(request):
         return render(request,'customerform/thanks.html')
     return render(request, 'customerform/index.html',{'form':form})
 
+def home(request):
+    return render(request,'home/home.html')
+
+
+def list(request):
+    name = PersonalData.objects.order_by('id')
+    return render(request,'list/all.html',{'name':name})
+
+
+def detail(request,user_id):
+    user = PersonalData.objects.get(pk=user_id)
+    return render(request,'list/detail.html',{'user':user})
